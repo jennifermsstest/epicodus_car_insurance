d1224c627c95d2f254e4871804cde3f4f1a602f7
Jennifer Marks and Sophia Fujiki <sophia@>
2019-02-18 18:47:43 -0800

    Add car insurance project

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  //Business Logic
+  $("form#insurance").submit(function(event){
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+  //User interface
+
+  if (age){
+    var quote = (100 - age) * 3;
+    if (gender === 'male' && age < 26){
+      quote += 50;
+    }
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+  } else {
+    alert('Please enter your age.');
+  }
+
+    event.preventDefault();
+  });
+});
